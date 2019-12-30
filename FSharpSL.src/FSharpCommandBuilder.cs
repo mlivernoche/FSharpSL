2d015007413fddcf6870996a105021fe24782840
Matthew Livernoche <mattalivernoche@gmail.com>
2019-12-30 09:07:44 -0800

    Removed old implementation.

--- FSharpSL.src/FSharpCommandBuilder.cs
+++ FSharpSL.src/FSharpCommandBuilder.cs
@@ -36,43 +36,6 @@ namespace FSharpSE
             return Commands.ToArray();
         }
 
-        internal static string[] CommandList(string filePath, IEnumerable<string> references)
-        {
-            var builder = new FSharpCompilerOptionsBuilder(filePath)
-            {
-                "--target:library",
-                "--highentropyva+",
-                "--tailcalls+"
-            };
-
-            var list = new HashSet<string>()
-            {
-                "fsc.exe",
-                "-a", filePath,
-                "-O",
-                "--target:library",
-                "--highentropyva+",
-                "--tailcalls+",
-                "--crossoptimize+",
-                "--platform:x64",
-                "--noframework",
-                "--simpleresolution",
-                "-r:" + Path.Combine(Environment.CurrentDirectory, "mscorlib.dll"),
-                "-r:" + Path.Combine(Environment.CurrentDirectory, "System.Private.CoreLib.dll"),
-                "-r:" + Path.Combine(Environment.CurrentDirectory, "System.Runtime.dll"),
-                "-r:" + Path.Combine(Environment.CurrentDirectory, "FSharp.Core.dll"),
-                "-r:" + Path.Combine(Environment.CurrentDirectory, "System.Globalization.dll"),
-                "-r:" + Path.Combine(Environment.CurrentDirectory, "System.Collections.dll")
-            };
-
-            foreach (var reference in references)
-            {
-                list.Add($"-r:{reference}");
-            }
-
-            return list.ToArray();
-        }
-
         public IEnumerator<string> GetEnumerator()
         {
             return ((IEnumerable<string>)Commands).GetEnumerator();
